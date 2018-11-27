be592999fd92bdb0ebadc9a8c7af16c87a55ac48
Adriano Augusto Tagliaferro <adrianotagliaferro@hotmail.com>
2018-11-27 11:52:18 -0200

    Criado Layout dos itens
    Criação do layout que será usado pelos itens da lista de usuários. Adição de algumas dependências remanescentes também.

--- app/src/main/java/tagliaferro/adriano/androiddev/MainActivity.kt
+++ app/src/main/java/tagliaferro/adriano/androiddev/MainActivity.kt
@@ -1,7 +1,7 @@
 package tagliaferro.adriano.androiddev
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
